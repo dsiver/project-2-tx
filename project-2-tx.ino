61241e04468d8ed72367975ff892285e6af565ac
David Siver <dsiver1@my.westga.edu>
2015-11-20 07:35:35 -0500

    Added method to turn Tx LED ON/OFF

--- project-2-tx.ino
+++ project-2-tx.ino
@@ -7,12 +7,28 @@
  * Fall 2015
  */
 
-void setup() {
-  // put your setup code here, to run once:
+#define TX_LED_PIN 4
+#define OFF "off"
+#define ON "on"
+#define NONE "none"
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(TX_LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void toggleLED(String mode, int pin) {
+  if (mode == ON) {
+    digitalWrite(pin, HIGH);
+    Serial.println("LED ON");
+  }
+  else if (mode == OFF) {
+    digitalWrite(pin, LOW);
+    Serial.println("LED OFF");
+  }
 }
