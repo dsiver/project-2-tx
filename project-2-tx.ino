bdbb5b8d8961effd7567bb1a2a41f90b415c08dd
David Siver <dsiver1@my.westga.edu>
2015-11-20 07:32:32 -0500

    initial commit

---
+++ project-2-tx.ino
@@ -0,0 +1,18 @@
+/*
+ * Project 2 Transmitter
+ * Board: Arduino UNO or similar with ATmega328P
+ *
+ * David Siver
+ * CS 4985
+ * Fall 2015
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
