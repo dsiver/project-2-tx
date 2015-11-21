826cd9f9438eaea955fee1422e60ef5c141a77a0
David Siver <dsiver1@my.westga.edu>
2015-11-21 15:21:39 -0500

    imported library, initialized driver

--- project-2-tx.ino
+++ project-2-tx.ino
@@ -7,16 +7,24 @@
  * Fall 2015
  */
 
+#include <RH_ASK.h>
+#include <SPI.h>
 #define TX_LED_PIN 4
 #define TX_PIN 5
 #define OFF "off"
 #define ON "on"
 #define NONE "none"
+#define MESSAGE 0x845FED
+
+RH_ASK txDriver;
 
 void setup() {
   Serial.begin(9600);
   pinMode(TX_LED_PIN, OUTPUT);
   pinMode(TX_PIN, INPUT);
+  if (!txDriver.init()){
+         Serial.println("TX driver init failed");
+  }
 }
 
 void loop() {
