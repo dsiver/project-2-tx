daf4c685ddc7e31729b23ddbc3d59bc71a45cae6
David Siver <dsiver1@my.westga.edu>
2015-11-25 16:11:04 -0500

    created body of loop to send a message. Printing to serial. Need to check against receiver

--- project-2-tx.ino
+++ project-2-tx.ino
@@ -6,6 +6,8 @@
  * CS 4985
  * Fall 2015
  */
+
+#include <VirtualWire.h>
 #define TX_LED_PIN 4
 #define TX_PIN 5
 #define TX_BUTTON_PIN 2
@@ -18,16 +20,26 @@ int txButtonState = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(TX_LED_PIN, OUTPUT);
-  pinMode(TX_PIN, OUTPUT);
+  //pinMode(TX_PIN, OUTPUT);
   pinMode(TX_BUTTON_PIN, INPUT);
+  vw_set_ptt_inverted(true);
+  vw_setup(2000);
+  vw_set_tx_pin(5);
 }
 
 void loop() {
+  char *message;
   txButtonState = digitalRead(TX_BUTTON_PIN);
-  if(txButtonState == HIGH){
+  if (txButtonState == HIGH && !vw_tx_active()) {
     toggleLED(ON, TX_LED_PIN);
+    char *message = "1";
+    Serial.print("Sending message: ");
+    Serial.println(message);
+    vw_send((uint8_t *)message, strlen(message));
+    vw_wait_tx();
+    toggleLED(OFF, TX_LED_PIN);
   }
-  else{
+  else {
     toggleLED(OFF, TX_LED_PIN);
   }
 }
