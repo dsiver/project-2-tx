cc06f488c4c78318e574ecda3740615a5574bcb6
David Siver <dsiver1@my.westga.edu>
2015-11-24 22:19:25 -0500

    removed radio head tx

--- project-2-tx.ino
+++ project-2-tx.ino
@@ -6,47 +6,30 @@
  * CS 4985
  * Fall 2015
  */
-
-#include <RH_ASK.h>
-#include <SPI.h>
 #define TX_LED_PIN 4
 #define TX_PIN 5
 #define OFF "off"
 #define ON "on"
 #define NONE "none"
 
-RH_ASK txDriver;
-const char *message = "ADAStx1";
 
 void setup() {
   Serial.begin(9600);
   pinMode(TX_LED_PIN, OUTPUT);
-  pinMode(TX_PIN, INPUT);
-  if (!txDriver.init()) {
-    Serial.println("TX driver init failed");
-  }
+  pinMode(TX_PIN, OUTPUT);
 }
 
 void loop() {
-  testTransmitter();
-}
-
-void testTransmitter() {
-  txDriver.send((uint8_t *)message, strlen(message));
-  txDriver.waitPacketSent();
-  toggleLED(ON, TX_LED_PIN);
-  delay(100);
-  toggleLED(OFF, TX_LED_PIN);
-  delay(60000);
+  
 }
 
 void toggleLED(String mode, int pin) {
   if (mode == ON) {
     digitalWrite(pin, HIGH);
-    Serial.println("LED ON");
+    //Serial.println("LED ON");
   }
   else if (mode == OFF) {
     digitalWrite(pin, LOW);
-    Serial.println("LED OFF");
+    //Serial.println("LED OFF");
   }
 }
