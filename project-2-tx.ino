8d653f124c632e7581269ac7eff4061db0f0b36a
David Siver <dsiver1@my.westga.edu>
2015-11-25 15:50:57 -0500

    Added button to make transmission send.

--- project-2-tx.ino
+++ project-2-tx.ino
@@ -8,19 +8,28 @@
  */
 #define TX_LED_PIN 4
 #define TX_PIN 5
+#define TX_BUTTON_PIN 2
 #define OFF "off"
 #define ON "on"
 #define NONE "none"
 
+int txButtonState = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(TX_LED_PIN, OUTPUT);
   pinMode(TX_PIN, OUTPUT);
+  pinMode(TX_BUTTON_PIN, INPUT);
 }
 
 void loop() {
-  
+  txButtonState = digitalRead(TX_BUTTON_PIN);
+  if(txButtonState == HIGH){
+    toggleLED(ON, TX_LED_PIN);
+  }
+  else{
+    toggleLED(OFF, TX_LED_PIN);
+  }
 }
 
 void toggleLED(String mode, int pin) {
