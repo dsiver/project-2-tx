64b0f1d8ab1577ed824fa04f54e13405a82fb5fc
David Siver <dsiver1@my.westga.edu>
2015-11-25 18:15:23 -0500

    sending transmission, need to try and make it only send one time.

--- project-2-tx.ino
+++ project-2-tx.ino
@@ -11,26 +11,28 @@
 #define TX_LED_PIN 4
 #define TX_PIN 5
 #define TX_BUTTON_PIN 2
+#define TX_BAUD 2000
 #define OFF "off"
 #define ON "on"
 #define NONE "none"
+#define DEBUG_BAUD 9600
 
 int txButtonState = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(DEBUG_BAUD);
   pinMode(TX_LED_PIN, OUTPUT);
   //pinMode(TX_PIN, OUTPUT);
   pinMode(TX_BUTTON_PIN, INPUT);
   vw_set_ptt_inverted(true);
-  vw_setup(2000);
-  vw_set_tx_pin(5);
+  vw_setup(TX_BAUD);
+  vw_set_tx_pin(TX_PIN);
 }
 
 void loop() {
   char *message;
   txButtonState = digitalRead(TX_BUTTON_PIN);
-  if (txButtonState == HIGH && !vw_tx_active()) {
+  if (txButtonState == HIGH) {
     toggleLED(ON, TX_LED_PIN);
     char *message = "1";
     Serial.print("Sending message: ");
@@ -47,10 +49,8 @@ void loop() {
 void toggleLED(String mode, int pin) {
   if (mode == ON) {
     digitalWrite(pin, HIGH);
-    //Serial.println("LED ON");
   }
   else if (mode == OFF) {
     digitalWrite(pin, LOW);
-    //Serial.println("LED OFF");
   }
 }
